80a843daef2fb46dfcdba72e1848de0010810dae
Andre Eikmeyer <andre.eikmeyer@gmail.com>
2026-06-28 19:28:17 +0200

    installation: rebuild initramfs correctly for i915

--- scripts/fedora/rebuild-initramfs.sh
+++ scripts/fedora/rebuild-initramfs.sh
@@ -8,8 +8,9 @@ require_fedora
 require_command dracut
 
 KVER="$(kernel_release)"
+ADD_DRIVERS="t2gmux i915"
 
 info "rebuilding initramfs for $KVER"
-dracut --force "/boot/initramfs-$KVER.img" "$KVER"
+dracut --force --add-drivers "$ADD_DRIVERS" "/boot/initramfs-$KVER.img" "$KVER"
 
 info "initramfs rebuilt"
